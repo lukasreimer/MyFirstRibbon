2ec5aad955d75d7de17b07a7966bc74b0e3d9ffe
Lukas Reimer <lukas.reimer@burohappold.com>
2019-03-12 19:51:11 +0100

    Updated Imports

--- MyFirstRibbon/CsAddPanel.cs
+++ MyFirstRibbon/CsAddPanel.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System.Windows.Media.Imaging;
